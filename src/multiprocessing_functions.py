abca275fb02eabfe7c40300b46021a7bbf2aef34
girstmairflorian-rgb <girstmairflorian@gmail.com>
2025-12-12 00:15:16 +0100

    Update type hints

--- src/multiprocessing_functions.py
+++ src/multiprocessing_functions.py
@@ -1,21 +1,23 @@
 from multiprocessing import Pool
 from tqdm import tqdm
 import math
+from typing import Callable, Any
 
-def pool_apply(numbers: list[int], algorithm, n_processes: int) -> list[int]:
+
+def pool_apply(numbers: list[int], algorithm: Callable, n_processes: int) -> list[Any]:
     with Pool(processes=n_processes) as pool:
         return [pool.apply(algorithm, args=(n,)) for n in tqdm(numbers)]
 
 
-def pool_apply_async(numbers: list[int], algorithm, n_processes: int) -> list[int]:
+def pool_apply_async(numbers: list[int], algorithm: Callable, n_processes: int) -> list[Any]:
     with Pool(processes=n_processes) as pool:
         results = [pool.apply_async(algorithm, args=(n,)) for n in tqdm(numbers)]
         return [result.get() for result in tqdm(results)]
 
 
-def pool_apply_async_chunked(numbers, algorithm, n_processes: int):
-    total = len(numbers)
-    chunk_size = auto_chunk_size(total)
+def pool_apply_async_chunked(numbers, algorithm: Callable, n_processes: int) -> list[Any]:
+    total: int = len(numbers)
+    chunk_size: int = auto_chunk_size(total)
     chunks = [numbers[i:i + chunk_size] for i in range(0, total, chunk_size)]
 
     with Pool(processes=n_processes) as pool:
@@ -23,23 +25,23 @@ def pool_apply_async_chunked(numbers, algorithm, n_processes: int):
         return [f.get() for f in tqdm(futures)]
 
 
-def pool_map(numbers: list[int], algorithm, n_processes: int) -> list[int]:
-    total = len(numbers)
-    chunk_size = auto_chunk_size(total)
+def pool_map(numbers: list[int], algorithm: Callable, n_processes: int) -> list[Any]:
+    total: int = len(numbers)
+    chunk_size: int = auto_chunk_size(total)
     with Pool(processes=n_processes) as pool:
         return pool.map(algorithm, tqdm(numbers), chunksize=chunk_size)
 
 
-def single_process_loop(numbers: list[int], algorithm, n_processes: int = None) -> list[int]:
+def single_process_loop(numbers: list[int], algorithm: Callable, n_processes: int = None) -> list[Any]:
     return [algorithm(n) for n in tqdm(numbers)]
 
 
-def auto_chunk_size(total_items, target_chunks=1000):
+def auto_chunk_size(total_items: int, target_chunks=1000) -> int:
     """Compute an efficient chunk size."""
     if total_items <= 0:
         return 1
 
-    chunk = math.ceil(total_items / target_chunks)
+    chunk: int = math.ceil(total_items / target_chunks)
 
     if chunk < 1:
         chunk = 1
@@ -49,5 +51,5 @@ def auto_chunk_size(total_items, target_chunks=1000):
     return chunk
 
 
-def run_chunk(chunk, algorithm):
+def run_chunk(chunk: Any, algorithm: Callable) -> list[int]:
     return [algorithm(n) for n in chunk]
